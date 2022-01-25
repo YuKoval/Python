3292213cdeb61474ea9335a6e7d3d1c332c14e87
Yuliia <97470242+YuKoval@users.noreply.github.com>
2022-01-25 20:02:18 +0200

    Update Arithmetic.py

--- Arithmetic.py
+++ Arithmetic.py
@@ -7,53 +7,44 @@ item_1 = 10
 item_2 = 11
 
   # 3. Создать переменную result_sum в которой вы суммируете item_1 и item_2
-result_sum = item_1 + item_2
-
   # 4. Вывести result_sum в консоль
+result_sum = item_1 + item_2
 print(result_sum)
 
   # 5. Создать переменную result_subtr в которой вы вычитаете большую по значению переменную из меньшей по значению
+  # 6. Вывести result_subtr в консоль
 result_subtr = item_2 - item_1
-
-  # Вывести result_subtr в консоль
 print(result_subtr)
 
-  # Создать переменную result_multi в которой вы умножаете item_1 на item_2
-result_multi = item_2 * item_1
-
+  # 7. Создать переменную result_multi в которой вы умножаете item_1 на item_2
   # 8. Вывести result_multi в консоль
+result_multi = item_2 * item_1
 print(result_multi)
 
   # 9. Создать переменную result_exp в которой вы item_1 возводите в степень item_2
-result_exp = item_2 ** item_1
-
   # 10. Вывести result_exp в консоль
+result_exp = item_2 ** item_1
 print(result_exp)
 
   # 11. Создать переменную result_m_exp в которой вы item_1 возводите в степень item_2 используя библиотеку math
+  # 12. Вывести result_m_exp в консоль.
 result_m_exp = math.pow(item_2, item_1)
 result_m_exp = math.pow(10,11)
-
-  # 12. Вывести result_m_exp в консоль.
 print(result_m_exp)
 
   # 13. Создать переменную result_s_root в которой вы найдёте квадратный корень любой из переменной item
-result_s_root = item_1 ** 0.5
-      # Любое число, возведенное в степень 0.5, дает нам квадратный корень из этого числа
-
   #  14. Вывести result_s_root в консоль
+result_s_root = item_1 ** 0.5       # Любое число, возведенное в степень 0.5, дает нам квадратный корень из этого числа
 print(result_s_root)
 
   # 15. Создать переменную result_m_s_root в которой вы найдёте квадратный корень любой из переменной item используя библиотеку math
-result_m_s_root = math.sqrt(item_1)
-
   # 16. Вывести result_m_s_root в консоль
+result_m_s_root = math.sqrt(item_1)
 print(result_m_s_root)
 
   # 17. Создать переменную result_mp_s_root в которой вы найдёте квадратный корень любой из переменной item используя библиотеку math используя метод pow
-result_mp_s_root = pow(item_1, 0.5)
-
   # 18. Вывести result_mp_s_root в консоль
+result_mp_s_root = pow(item_1, 0.5)
 print(result_mp_s_root)
 
   # 19. Присвоить переменной item_1 odd значение
@@ -62,42 +53,38 @@ item_1 = 77
 item_2 = 22
 
   # 21. Создать переменную result_division в которой вы разделите item_1 на item_2
-result_division = item_1 / item_2
-
   # 22. Вывести result_division в консоль
+result_division = item_1 / item_2
 print(result_division)
 
   # 23. Создать переменную result_m_floor и result_division округлить до ближайшего целого меньшего чем result_division
-result_m_floor =  math.floor(result_division)
-
   # 24. Вывести result_m_floor в консоль.
+result_m_floor =  math.floor(result_division)
 print(result_m_floor)
 
   # 25. Создать переменную result_m_ceil и result_division округлить до ближайшего целого большего чем result_division
-result_m_ceil = math.ceil(result_division)
-
   # 26. Вывести result_m_ceil в консоль
+result_m_ceil = math.ceil(result_division)
 print(result_m_ceil)
 
   # 27. Создать переменную result_int и result_division округлить до ближайшего целого через явное приведение
-result_int = int(result_division)
-
   # 28. Вывести result_int в консоль
+result_int = int(result_division)
 print(result_int)
 
   # 29. Создать переменную result_no_division_loss в которой вы разделите item_1 на item_2 без остатка
-result_no_division_loss = item_1 // item_2
-
   # 30. Вывести result_no_division_loss в консоль
+result_no_division_loss = item_1 // item_2
 print(result_no_division_loss)
 
   # 31. Создать переменную result_division_loss в которой вы  найдёте остаток от деления item_1 на item_2
-result_division_loss = item_1 % item_2
-
   # 32. Вывести result_division_loss в консоль
+result_division_loss = item_1 % item_2
 print(result_division_loss)
 
-        # Pеализация через арифметические действия с присваиванием.
+
+# Pеализация через арифметические действия с присваиванием.
+
 
   # 33. Создать переменную item_3 и присвоить ей целое число
 item_3 = 22
