5d95a442ccbc810723057ea8574eaffb081b20a1
YuKoval <uliakovaluliakoval>
2022-01-25 23:31:03 +0200

    done

--- Arithmetic.py
+++ Arithmetic.py
@@ -83,71 +83,81 @@ result_division_loss = item_1 % item_2
 print(result_division_loss)
 
 
-# Pеализация через арифметические действия с присваиванием.
+# Pеализация через арифметические действия с присваиванием
 
 
   # 33. Создать переменную item_3 и присвоить ей целое число
 item_3 = 22
 
   # 34. Прибавить 10 к item_3 с присвоением
-item_3 = item_3 + 10
-
   # 35. Вывести item_3 в консоль
-print("item_3 =",item_3)
+item_3 += 10
+print("item_3 '+= 10' = ",item_3)
 
-  # 36. Отнять 5 от item_3 с присвоением.
-item_3 = item_3 - 5
+  # 36. Отнять 5 от item_3 с присвоением
+  # 37. Вывести item_3 в консоль
+item_3 -= 5
+print("item_3 '-= 5' = ",item_3)
 
-# 37. Вывести item_3 в консоль
-print("item_3 =",item_3)
+  # 38. Умножить item_3 на 3 с присвоением
+  # 39. Вывести item_3 в консоль
+item_3 *= 3
+print("item_3 '*=3' = ",item_3)
 
-  # 38. Умножить item_3 на 3 с присвоением. 39. Вывести item_3 в консоль
-item_3 = item_3 * 3
-print("item_3 =",item_3)
+  # 40. Разделить item_3 на 2 с присвоением
+  # 41. Вывести item_3 в консоль
+item_3 /= 2
+print("item_3 '/=2' = ", item_3)
 
-  # 40. Разделить item_3 на 2 с присвоением. 41. Вывести item_3 в консоль
-item_3 = item_3 / 2
-print('item_3 =', item_3)
+  # 42. Возвести в степень 2 item_3 с присвоением
+  # 43. Вывести item_3 в консоль
+item_3 **= 2
+print("item_3 '**=2' = ", item_3)
 
-  # 42. Возвести в степень 2 item_3 с присвоением. 43. Вывести item_3 в консоль
-item_3 = item_3 ** 2
-print('item_3 =', item_3)
+  # 44. Найти квадратный корень item_3 с присвоением
+  # 45. Вывести item_3 в консоль
+item_3 **= 0.5
+print("item_3 '**=0.5' = ", item_3)
 
-  # 44. Найти квадратный корень item_3 с присвоением. 45. Вывести item_3 в консоль
-item_3 = item_3 ** 0.5
-print("item_3 =",item_3)
+  # 46. Присвоить остаток от деления item_3
+  # 47. Вывести item_3 в консоль
+item_3 %= item_3
+print("item_3 '%=item_3' = ", item_3)
 
-  # 46. Присвоить остаток от деления item_347. Вывести item_3 в консоль
-item_3 = item_3 % item_3
-print("item_3 =",item_3)
-            # Boolean
+# Boolean
 
   # 48. Создать переменную b_item_t и присвоить True
   # 49. Создать переменную b_item_f и присвоить False
 b_item_t = True
 b_item_f = False
 
-  # 50. Создать переменную b_item_relult_sum и присвоить сумму b_item_t и b_item_f. 51. Вывести b_item_relult_sum в консоль
+  # 50. Создать переменную b_item_relult_sum и присвоить сумму b_item_t и b_item_f
+  # 51. Вывести b_item_relult_sum в консоль
 b_item_relult_sum = b_item_t + b_item_f
-print(b_item_relult_sum)
+print('b_item_relult_sum = ', b_item_relult_sum)
 
-  # 52. Создать переменную b_item_relult_subtr и присвоить разницу b_item_t и b_item_f. 53. Вывести b_item_relult_subtr в консоль.
+  # 52. Создать переменную b_item_relult_subtr и присвоить разницу b_item_t и b_item_f
+  # 53. Вывести b_item_relult_subtr в консоль.
 b_item_relult_subtr = b_item_t - b_item_f
-print(b_item_relult_subtr)
+print('b_item_relult_subtr = ', b_item_relult_subtr)
 
 
-  # 54. Создать переменную b_item_relult_multi и присвоить умножение b_item_t и b_item_f. 55. Вывести b_item_relult_multi в консоль.
+  # 54. Создать переменную b_item_relult_multi и присвоить умножение b_item_t и b_item_f
+  # 55. Вывести b_item_relult_multi в консоль.
 b_item_relult_multi = b_item_t * b_item_f
-print(b_item_relult_multi)
+print('b_item_relult_multi = ', b_item_relult_multi)
 
-   # 56. Создать переменную b_item_relult_division и присвоить деление b_item_t и b_item_f. 57. Вывести b_item_relult_division в консоль. (Получить ошибку)
+   # 56. Создать переменную b_item_relult_division и присвоить деление b_item_t и b_item_f
+   # 57. Вывести b_item_relult_division в консоль. (Получить ошибку)
 b_item_relult_division = b_item_t / b_item_f
-print(b_item_relult_division)
+print('b_item_relult_division = ', b_item_relult_division)
 
-  # 58. Создать переменную b_item_1_int и присвоить явное приведение b_item_t к int. 59. Вывести b_item_1_int в консоль
+  # 58. Создать переменную b_item_1_int и присвоить явное приведение b_item_t к int
+  # 59. Вывести b_item_1_int в консоль
 b_item_1_int = int(b_item_t)
-print(b_item_1_int)
+print('b_item_1_int = ', b_item_1_int)
 
-  # 60. Создать переменную b_item_2_int и присвоить явное приведение b_item_2 к int. 61. Вывести b_item_2_int в консоль
+  # 60. Создать переменную b_item_2_int и присвоить явное приведение b_item_2 к int
+  # 61. Вывести b_item_2_int в консоль
 b_item_2_int = int (b_item_f)
-print(b_item_2_int)
+print('b_item_2_int = ', b_item_2_int)
